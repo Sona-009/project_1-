87f909f2bcacf9f6f9aee7d87080eb7c6d0e5ef6
Sahana <sahananadagouda30@gmail.com>
2025-03-24 17:16:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let countChild = document.getElementById("countChild");
+
+let btn = document.getElementById("clickBtn");
+let btn2 = document.getElementById('clickBtnneg');
+let count = 0;
+
+btn.addEventListener('click',()=>{
+    count++;
+    countChild.innerHTML = `<h1>Count: ${count} </h1>`;
+})
+
+btn2.addEventListener('click',()=>{
+    count--;
+    countChild.innerHTML = `<h1>Count: ${count} </h1>`;
+})
